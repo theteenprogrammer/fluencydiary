2ee66f7ea2456899daf23e09067da65e3f3a4004
Code and Acquire <63100216+theteenprogrammer@users.noreply.github.com>
2021-11-14 18:49:24 +0000

    Initial laying everything out

---
+++ editor.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<!-- some css to format the title and textarea a bit -->
+<style>
+</style>
+
+<!-- php include stuff -->
+<?php 
+
+?>
+</head>
+
+<body>
+<!-- what I can see -->
+<input type="text" name="title" />
+<textarea></textarea>
+<input type="submit" name="publish" value="Publish" />
+</body>
+
+<!-- php stuff cuz there's plenty of space atm n I can understand it easier for now -->
+<?php 
+
+//check that it's all submitted
+
+//make sure my database doesn't get hacked
+
+//prepare a statement or an INSERT
+
+//the end
+?>
+</html>
